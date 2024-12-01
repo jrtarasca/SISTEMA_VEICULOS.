2e559d9e4ecf782ab3b785bab0ce11c26b1f9cd3
jrtarasca <166664327+jrtarasca@users.noreply.github.com>
2024-12-01 16:50:17 -0300

    Update ConectDb.java

--- SistemaVeiculos/src/com/aulas/sistemaVeiculos/ConectDb.java
+++ SistemaVeiculos/src/com/aulas/sistemaVeiculos/ConectDb.java
@@ -5,18 +5,18 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class ConectDb {
-    // Definindo parâmetros para a conexão
+   
     private static final String URL = "jdbc:mysql://localhost:3306/veiculos";
     private static final String USUARIO = "root";
-    private static final String SENHA = ""; // Coloque a senha do seu MySQL, se houver
+    private static final String SENHA = "";
 
     // Método para obter conexão com o banco
     public static Connection conectar() {
         Connection conexao = null;
         try {
-            // Carregando o driver JDBC
+            //  driver JDBC
             Class.forName("com.mysql.cj.jdbc.Driver");
-            // Tentando estabelecer conexão
+           
             conexao = DriverManager.getConnection(URL, USUARIO, SENHA);
             System.out.println("Conexão realizada com sucesso!");
         } catch (ClassNotFoundException e) {
